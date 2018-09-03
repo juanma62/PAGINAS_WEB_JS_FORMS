23378c49a5b2a0567e627051ff91f857758f6f39
juanma62 <jsalazarmanuel@yahoo.es>
2018-09-03 14:00:02 +0200

    Cambios_clase_3-9-18

---
+++ 03_formulario/js/form.js
@@ -0,0 +1,20 @@
+export class Form{
+    constructor(){
+        this.domForm = document.querySelector('form')
+        this.domUser = document.querySelector('#user')
+        this.domPassw = document.querySelector('#passw')
+        this.data = {
+            user : '',
+            passw : ''
+        }
+        this.domForm.addEventListener('submit',
+            this.enviar.bind(this)
+        )
+    }
+    enviar(oEv){
+        oEv.preventDefault()
+        this.data.user = this.domUser.value
+        this.data.passw = this.domPassw.value
+        console.log(this.data)
+    }
+}
---
+++ 03_formulario/js/main.js
@@ -0,0 +1,7 @@
+import{Form} from './form.js'
+
+document.addEventListener('DOMContentLoaded', 
+    () => {
+        new Form()
+    }
+)
