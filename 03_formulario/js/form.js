5fb615f87e06f6c3c1870f80844b773ce868b4f2
juanma62 <jsalazarmanuel@yahoo.es>
2018-09-04 11:10:07 +0200

    Cambios_descanso

--- 03_formulario/js/form.js
+++ 03_formulario/js/form.js
@@ -1,11 +1,20 @@
 export class Form{
     constructor(){
         this.domForm = document.querySelector('form')
-        this.domUser = document.querySelector('#user')
         this.domPassw = document.querySelector('#passw')
         this.data = {
-            user : '',
-            passw : ''
+            user : {
+                name : '',
+                apellidos : ''
+            },
+            email : '',
+            passw : '',
+            coments : '',
+            opciones : {
+                condiciones : '',
+                turno : '',
+                curso : ''
+            }
         }
         this.domForm.addEventListener('submit',
             this.enviar.bind(this)
@@ -13,8 +22,38 @@ export class Form{
     }
     enviar(oEv){
         oEv.preventDefault()
-        this.data.user = this.domUser.value
-        this.data.passw = this.domPassw.value
-        console.log(this.data)
+        this.data.user.name = document.querySelector('#nombre').value
+        this.data.user.apellidos = document.querySelector('#apellidos').value
+        this.data.email = document.querySelector('#correo').value
+        this.data.passw = document.querySelector('#passw').value
+        this.data.coments = document.querySelector('#comentarios').value
+        this.data.opciones.condiciones = document.querySelector('#condiciones').checked
+        this.data.opciones.turno = this.verTurno()
+        /* this.data.opciones.curso = document.querySelector('#condiciones').value */
+        
+        console.dir(this.data)
+    }
+
+    verTurno(){
+        let aTurnos = document.getElementsByName('turno')
+        //let aTurnos = document.querySelectorAll('[name="turno"]')
+        /* for (let i = 0; i < aTurnos.length; i++) {
+            const item = aTurnos[i];
+            if(item.checked){
+                return item.value
+            }      
+        } */
+
+        let resultado = ''
+
+        aTurnos.forEach(
+            item => {
+                if(item.checked){
+                    resultado = item.value
+                    return true
+                }
+            }
+        )
+        return resultado
     }
 }
