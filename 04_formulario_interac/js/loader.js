08500befceb5187d24afd84b68f6ae48907e6df4
juanma62 <jsalazarmanuel@yahoo.es>
2018-09-04 13:59:05 +0200

    Cambios_4-5-18_final

---
+++ 04_formulario_interac/js/loader.js
@@ -0,0 +1,10 @@
+class Loader {
+    constructor(){
+        this.cambia = document.querySelector('#mas')
+    }
+    this.cambia.addEventListener('click', 
+            this.escribir.bind(this))
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+})
