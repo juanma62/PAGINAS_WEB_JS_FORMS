912b24bcdbdcbf7ee8e7598c45df1e370f4647fa
juanma62 <jsalazarmanuel@yahoo.es>
2018-09-27 13:57:18 +0200

    Cambios_27-9-18_clase

--- angular2/src/app/tareas-simple/tareas-simple.component.ts
+++ angular2/src/app/tareas-simple/tareas-simple.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasSimpleComponent implements OnInit {
 
+  aTareas: Array<string>
+  sTarea: string
+
   constructor() { }
 
   ngOnInit() {
+    // this.aTareas = []. Otra forma de ponerlo sin else
+    let saveTareas = JSON.parse(localStorage.getItem('tareasStorage'))
+    if(saveTareas){
+      this.aTareas = saveTareas
+    } else{
+      this.aTareas = []
+    }
   }
 
+  addTarea(){
+    let stringTareas: string
+    if(this.sTarea){
+      this.aTareas.push(this.sTarea)
+      stringTareas = JSON.stringify(this.aTareas)
+      localStorage.setItem('tareasStorage', stringTareas)
+      this.sTarea = ''
+    }
+    console.log(this.aTareas)
+  }
 }
