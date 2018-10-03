9d731df0ab3d42d114ce95eafd2ee4b5b9650648
juan <juan@iMac-de-juan.local>
2018-10-03 23:34:45 +0200

    Cambios_3-10-18_casa

--- angular2/src/app/contacts/contacts.component.ts
+++ angular2/src/app/contacts/contacts.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cas-contacts',
-  template: '<cas-lista-contactos></cas-lista-contactos>',
+  template: '<cas-lista-contactos class="row"></cas-lista-contactos>',
   styles: []
 })
 export class ContactsComponent implements OnInit {
