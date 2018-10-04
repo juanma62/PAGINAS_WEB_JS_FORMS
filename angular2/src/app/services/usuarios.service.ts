275a7749cec3e0567e69f4d5d9f4129499b4f0bb
juanma62 <jsalazarmanuel@yahoo.es>
2018-10-04 13:59:57 +0200

    Cambios_4-10-18_clase

--- angular2/src/app/services/usuarios.service.ts
+++ angular2/src/app/services/usuarios.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { ContactoIf } from '../models/contacto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  constructor(public http: HttpClient) { }
+  aContactos: Array<ContactoIf>
+  url: string
+
+  constructor(public http: HttpClient) {
+    this.aContactos = []
+    this.url = environment.api_users
+   }
+
+  getLista(){
+    return new Promise(
+      (resolve, reject) => {
+        this.http.get(this.url).toPromise()
+        .then((response: any) => {
+          this.aContactos = response.results
+          resolve(this.aContactos)
+        })
+      }
+    )  
+  }
 }
